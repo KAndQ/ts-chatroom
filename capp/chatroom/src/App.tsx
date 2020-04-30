3a820edb3436277b0a9a7ce47a7c6bcdd89228d2
yaozhenyu <609045030@qq.com>
2020-04-30 13:27:30 +0800

    add PingApp demo.

--- capp/chatroom/src/App.tsx
+++ capp/chatroom/src/App.tsx
@@ -1,24 +1,12 @@
 import React, { Component } from "react";
 import Button from "antd/es/button";
 import "./App.css";
-import SocketIO from "socket.io-client";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Button type="primary" onClick={() => {
-                    console.log("click");
-
-                    let socket = SocketIO("http://127.0.0.1:48081")
-                        .on("connect", () => {
-                            console.log("[SOCKETIO CONNECT]");
-                            socket.emit("data", "ping");
-                        })
-                        .on("data", (data: any) => {
-                            console.log("[SOCKETIO DATA]: " + data);
-                        });
-                }}>
+                <Button type="primary" onClick={() => {}}>
                     Button
                 </Button>
             </div>
---
+++ capp/chatroom/src/demo/PingApp.tsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import "../App.css";
+import SocketIO from "socket.io-client";
+import * as binconv from 'binconv';
+
+interface IState {
+    count: number;
+}
+
+export default class PingApp extends Component<any, IState> {
+    public constructor(props: any) {
+        super(props);
+
+        this.state = {
+            count: 0,
+        };
+    }
+
+    componentDidMount() {
+        let socket = SocketIO("http://127.0.0.1:48081")
+            .on("connect", () => {
+                console.log("[SOCKETIO CONNECT]");
+
+                setInterval(() => {
+                    socket.emit("data", "ping");
+                    this.setState({ count: this.state.count + 1 });
+                }, 1000);
+                socket.emit("data", "ping");
+                this.setState({ count: this.state.count + 1 });
+            })
+            .on("data", (data: any) => {
+                if (data instanceof String) {
+                    console.log("[SOCKETIO STRING DATA]: " + data);
+                } else if (data instanceof ArrayBuffer) {
+                    console.log("[SOCKETIO ARRAYBUFFER DATA]: " + binconv.uint8ArrayToString(new Uint8Array(data)));
+                }
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>PingApp Demo</h1>
+                count = {this.state.count}
+            </div>
+        );
+    }
+}
